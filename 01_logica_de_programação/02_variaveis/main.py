1d70b370fc3356a78f899b952e32482a3de2138c
mique-devgit config user.name mique-devgit config user,email miqueias.gt867@gmail.com <miqueias.gt867@gmail.com>
2025-10-20 17:36:34 -0300

    first commit

---
+++ 01_logica_de_programação/02_variaveis/main.py
@@ -0,0 +1,10 @@
+# declaração de variáveis
+nome_completo = "Mqueis Costa" # string
+idade =19 # int
+altura =1.75 # float
+programador = True
+
+print(nome_completo)
+print(idade)
+print(altura)
+print(programador)
---
+++ 01_logica_de_programação/03_tipos_de_dados/main.py
@@ -0,0 +1,11 @@
+#
+nome = ("Miqueias costa")
+idade =19
+altura = 1.75
+programador = True
+
+# exibe os tipos de dados
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(programador))
