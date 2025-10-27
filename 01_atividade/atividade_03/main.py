38010bc0959394c1c1c1a0ea99215947f0ecccc9
mique-dev <miqueias.gt867@gmail.com>
2025-10-27 17:34:17 -0300

    atividade 27/10/25

--- 01_atividade/atividade_03/main.py
+++ 01_atividade/atividade_03/main.py
@@ -1,17 +1,35 @@
-# entrada de dados
-nome = input("Informe seu nome").strip().title()
-peso = input(float("Informe seu peso")).strip()
-altura = input(float("Informe sua altura")).strp()
-IMC = round(peso/altura**2,2)
-ptint = ("Informe {IMC}")
+# TODO: atividade 
 
-if IMC <= 18.0:
-    print("Voçê esta muuuito magro. ")
-elif IMC <= 25.0:
-    print("Voçê esta com um peso bacana. ")
-elif IMC <= 35.0:
-    print("Voçê esta com um peso noemal. ")
-elif IMC <= 40.9:
-    print("Acho q voçê ganhou um pouquinho de peso. ")
-else:
-    print("Por favor. Não pule de um lugares muito altos. vai quebrar o planeta. ")
+# tratamento de exceção
+
+try:
+    # entrada de dados
+    
+    nome = input("Informe seu nome: ").strip().title()
+    peso = float(input("Informe seu peso: ").strip().replace(",","."))
+    altura = float(input("Informe sua altura em metros: ").strip().replace(",","."))
+    
+    # calcule do IMC
+
+    IMC = peso / (altura ** 2)
+
+    # exibe o imc do usuario
+    print (f"{nome}, seu IMC é: {IMC:.2f}")
+
+    # diagnóstico do IMC
+    if IMC < 18.0:
+       print(f"{nome} esta muuuito magro. ")      
+    if IMC < 25:
+       print(f"{nome} Voçê esta com o peso ideal. ")
+    if IMC < 30:
+       print(f"{nome} Voçê esta com sobrepeso. ")
+    elif IMC < 35:
+       print(f"{nome} esta com obesidade grau 1. ")
+    elif IMC < 40:
+       print(f"{nome} esta com obesidade grau 2. ")
+    else:
+       print(f"Por favor. Não pule de um lugares muito altos. vai quebrar o planeta. ")
+
+except Exception as e:
+    print(f"Por favor, insira valores válidos.{e}")
+
--- 01_atividade/atividade_04/main.py
+++ 01_atividade/atividade_04/main.py
@@ -4,10 +4,58 @@ Crie um programa que recebe do usuário o nome e a idade, e em seguida, exiba na
 C
 O usuário deverá escolher o filme, e caso ele tiver a idade mínima para ver o filme, imprime o ingresso e encerra o programa. Caso o usuário não tenha a idade mínima, o programa impede a entrada do usuário e re-exibe a lista de filmes para que o mesmo escolha outro filme.
 """
-
-nome = input("Informe seu nome: ").strip().title()
-idade = int(input("Informe sua idade: ").strip())
-input = int(input("ala 1 - A Roda Quadrada - Livre. Sala 2 - A Volta dos Que Não Foram - 12 anos. Sala 3 - Poeira em Alto Mar - 14 anos. Sala 4 - As Tranças do Rei Careca - 16 anos. Sala 5 - A Vingança do Frango Assado - 18 anos: ").strip())
-                
 try:
-    nome = 
+    nome = input("Informe seu nome: ").strip().title()
+    idade = int(input("Informe sua idade: ").strip())
+
+    # lista de filmes
+    sala_1 = " a roda quadrada"
+    sala_2 = " a volta dos que não foram"
+    sala_3 = " poeira em alto mar"
+    sala_4 = " o q importa é só o importante"
+    sala_5 = " oq está cheio não está vazio"
+
+    # loop
+    while True:
+        # menu filmes
+        print(f"sala 1 - {sala_1} -livre")
+        print(f"sala 2 - {sala_2} - 12 anos")
+        print(f"sala 3 - {sala_3} - 14 anos")
+        print(f"sala 4- {sala_4} - 16 anos")
+        print(f"sala 5 - {sala_5} - 18 anos")
+        sala = input(" Informe a sala que dessejada: ").strip().lower()
+
+        # verificação da sala
+        match sala:
+            case "1":
+                filme = sala_1
+                idade_minima = 0
+                pass
+            case "2":
+                filme = sala_2
+                idade_minima = 12
+                pass
+            case "3":
+                filme = sala_3
+                idade_minima = 14
+                pass
+            case "4":
+                filme = sala_4
+                idade_minima = 16
+                pass
+            case "5":
+                filme = sala_5
+                idade_minima = 18
+                pass
+            case _: 
+                print("esta sala n existe")
+        if idade >= idade_minima:
+            print(f"{nome} escolheu {filme} e pode assistir ao filme. ")
+            break
+        else:
+            print(f"{nome} não foi autorizado a ver {filme}.")
+            continue
+except Exception as e:
+    print(f"lamento mais fui de F.{e}")
+
+ 
