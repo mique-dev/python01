3d3d9b69858913b703b343df268c658b5356345f
mique-dev <miqueias.gt867@gmail.com>
2025-11-11 17:54:19 -0300

    atividade do dia 11/11/25

--- 01_atividade/atividade_07/classes.py
+++ 01_atividade/atividade_07/classes.py
@@ -1,18 +1,27 @@
-class Banco:
+import os
 
-    def __init__(self, nome, saldo, cpf, agencia, conta):
-        self.nome = nome
-        self.saldo = saldo
+class Conta:
+    def __init__(self, titular, cpf, n_agencia, n_conta, saldo):
+        self.nome = titular
         self.cpf = cpf
-        self.agencia = agencia
-        self.conta = conta
-        
-    def exibir_dados():
-        print("Nome: {self.nome}")
-        print("Saldo: {self.saldo}")
-        print("Cpf: {self.cpf}")
-        print("numero da agencia: {self.agencia}")
-        print("numero da conta: {self.conta}")
-        
-
-        def
+        self.saldo = saldo
+        self.agencia = n_agencia
+        self.conta = n_conta
+    
+    def consultar_dados(self):
+        print(f"Nome do titular da conts: {self.nome}")
+        print(f"CPF do titular: {self.cpf}")
+        print(f"Agencia da conta: {self.agencia}")
+        print(f"Numero da onta: {self.conta}")
+        print(f"Saldo da conta: R${self.saldo}")
+    
+    def depositar(self, valor):
+        self.saldo += valor
+        print(f"Deposito realizado com sucesso. Seu saldo atual é de R${self.saldo}")
+    
+    def sacar(self, valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+            return self.saldo
+        else:
+            print("Saldo insuficiente")
--- 01_atividade/atividade_07/main.py
+++ 01_atividade/atividade_07/main.py
@@ -1,56 +1,39 @@
-# biblioteca
 import os
-
-# limpar tela
-def limpar():
-    os.system("cls" if os .name == "nt" else "clear")
-
-# minha biblioteca
-from classes import banco
+from classes import Conta
+def limpar ():
+    os.system("cls" if os.name == "nt" else "clear")
 
 def main():
-    usuario = banco(nome="",cpf="", depositar="", sacar="", saldo="")
-
-# menu
-while True:
     limpar()
-    print("Bem vindo ao banco")
-    print("1 - Inserir os dados da conta")
-    print("2 - Depositar")
-    print("3 - Sacar")
-    print("4 - Saldo")
-    print("5 - Sair")
-    opcao = input("Digite uma opção para prosseguir: ")
-    
-    match opcao:
-        case "1":
-            limpar()
-            tiular = input("Informe o nome do titular: ").strip().title()
-            saldo = float(input("Informe o saldo da conta: ").strip().replace(",", "."))
-            cpf = input("Informe o CPF do titular: ").strip()
-            numero_da_agencia = input("Informe o numero da agencia: ").strip()
-            numero_da_conta = input("Informe o numero da conta: ").strip()
-            continue
-        case "2":
-            depositar = float(input("Informe o valor que deseja depositar: ").strip().replace(",", "."))
-            banco.depositar(depositar)
-            continue
-        case "3":
-            sacar = float(input("Informe o valor que deseja sacar: ").strip().replace(",", "."))
-            banco.sacar(sacar)
-            continue
-        case "4":
-            banco.saldo()
-            continue
-        case "5":
-            limpar()
-            print("Obrigado por usar nosso banco") 
-            break
-        case _:
-            limpar()
-            print("Opção inválida")
-            continue
 
+    cc = Conta(titular ="", cpf ="", n_agencia ="7462-2", n_conta ="8787-9", saldo ="")
+
+    cc.titular = input("Informe seu nome: ").strip().title()
+    cc.cpf = input("Informe seu CPF: ").strip()
+
+    while True:
+        print("1 - Consultar dados da conta")
+        print("2 - Depositar valor")
+        print("3 - Sacar valor")
+        print("4 - Sair do programa")
+        opcao = input("Informe a opção desejada: ").strip()
+        match opcao:
+            case "1":
+                cc.consultar_dados()
+                continue
+            case "2":
+                valorr = float(input("Informe o valor que deseja depositar: R$").strip().replace(",","."))
+                print(f"Deposito realizado com sucesso. Seu saldo atual é de R${cc.depositar(valor):.2F}")
+            case "3":
+                valor = float(input("Informe o valor que deseja sacar: R$").strip().replace(",","."))
+                print(f"Valor sacado: R${cc.sacar(valor):.2F}")
+                continue
+            case "4":
+                print("Programa emcerrado")
+                break
+            case _:
+                print("Opção inválida")
+                continue
 
 if __name__ == "__main__":
     main()
