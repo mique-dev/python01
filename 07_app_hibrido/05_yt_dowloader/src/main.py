957c9a4ddd85bf9ac97a8150a9760f60a5653088
mique-dev <miqueias.gt867@gmail.com>
2025-12-01 17:33:35 -0300

    aula do dia 01/12/25

---
+++ 07_app_hibrido/05_yt_dowloader/src/main.py
@@ -0,0 +1,124 @@
+import flet as ft
+from pytubefix import Youtube
+
+import threading
+
+
+def main(page: ft.Page):
+    page.title = "YouTube Downloader"
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.window.icon = "assets/youtube.png"
+
+    #criar as pastas caso não existam
+    caminho_videos = 'videos'
+    caminho_audios = 'audios'
+    os.makedirs(caminho_videos, exist_ok=True)
+    os.makedirs(caminho_audios, exist_ok=True)
+
+    # componentes de interface grafica
+    titulo = ft.Text("Use uma URL", color=ft.Colors.BLACK, size=20, weight=ft.FontWeight.BOLD)
+    url = ft.TextField(
+        label="Insira a URL do YouTube aqui",
+        width=400,
+        border_radius=10
+    )
+
+    base_path = os.path.dirname(__file__)
+    logo_path = os.path.join(base_path, "assets", "youtube.png")
+    logo.cabbecalho = ft.Image(src=logo_path, width=300, height=200)
+
+    # componente para mostrar infor,açoes do video
+    video_info = ft.Container(
+        content=ft.Column([]),
+        visible=False,
+        padding=10,
+        bgclor=ft.colors.BLUE_GREY_50,
+        border_radius=10,
+        width=400
+    )
+
+    # barra de progresso
+    progress_bar = ft.ProgressBar(
+        visible=False,
+        width=400,
+        color=ft.Colors.RED,
+        bgcolor=ft.Colors.BLUE_GREY_100
+    )
+    # testto de status
+    status_text = ft.Text(
+        "",
+        color=ft.Colors.BLACK,
+        size=14,
+        text_align=ft.TextAlign.CENTER
+    )
+    
+    def mostrar_info_video(yt):
+        try:
+            # limpar o cotainer
+            vido_info.content.controls.clear()
+
+            # adicionar informações do video
+            vide_info.conetnt.controls.extend(
+                [
+                    ft.Text(f"Título: {yt.title}", size=14, weight=ft.FontWeight.BOLD),
+                    ft.Text(f"Canal: {yt.title}", size=12),
+                    ft.Text(f"Título: {yt.lenth/60}:{yt.lenth%60:02d}" ,size=12),
+                    ft.Text(f"Título: {yt.views:,}" ,size=12),
+                ]
+            )
+
+            video_info.visible = True
+            page.update()
+
+        except Exception as e:
+            status_text.value = f"Erro ao obter informações do vídeo: {str(e)}"
+            status_text.color = ft.Colors.RED
+            page.update()
+
+    def baixar_video(e):
+        if not url .value.strip():
+            status_text.value = "Por favor, insira uma URL válida."
+            status_text.color = ft.Colors.ORANGE
+            page.update()
+        
+        def download_thread():
+            try:
+                # mostra progressão
+                progress_bar.visible = True
+                status_text.value = "Obtendo vídeo..."
+                status_text.color = ft.Colors.BLUE
+
+                # criar objeto Youtube
+                yt = Youtube(url.value.strip())
+                mostrar_info_video(yt)
+
+                # inicio do dowload
+                status_text.value = f"Baixando video... {yt.title}"
+                page.update()
+
+                # pega a maior resolução possivel
+                stream = yt.streams.get_highest_resolution()
+
+                # TODO: fazer if else do stream
+
+            except Exception as e:
+                progress_bar.visible = False
+                status_text.value = f"Erro: {str(e)}."
+                status_text.color = ft.Colors.RED
+                page.update()
+
+        # except e, thread separa para não travar a interface
+        threading.Thread(target=download_thread, daemon=True).start()
+
+    page.add(
+        ft.SafeArea(
+            ft.Container(
+                counter,
+                alignment=ft.alignment.center,
+            ),
+            expand=True,
+        )
+    )
+
+
+ft.app(main)
