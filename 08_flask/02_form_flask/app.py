96c1323188f1b349a0b3eda9ff5c1cf4c5ec00cd
mique-dev <miqueias.gt867@gmail.com>
2025-12-09 17:40:50 -0300

    frst ocmmit

--- 08_flask/02_form_flask/app.py
+++ 08_flask/02_form_flask/app.py
@@ -3,13 +3,14 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def index():return render_template("index.html")
+def index():
+    return render_template("index.html")
 
-@app.route("/enviar")
+@app.route("/enviar", mothods=["POST"])
 def enviar():
-    texto = request.form.get('texto')
+    texto = request.form.get('texto','')
     return render_template("index.html", saida=texto)
 
-j
+
 if __name__ == "__main__":
     app.run(debug=True)
